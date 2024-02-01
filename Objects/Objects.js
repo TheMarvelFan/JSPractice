1c566b13ec0604cd819d94eb4155f5b9eabad3b3
Kushagra Nigam <93257324+TheMarvelFan@users.noreply.github.com>
2024-02-01 20:52:17 +0530

    Object introduction

--- Objects/Objects.js
+++ Objects/Objects.js
@@ -23,9 +23,23 @@ console.log(user["key name"]); // cannot access space separated string key using
 console.log(user[sym1]);
 
 user.age = 23;
-Object.freeze(user); // further changes to any values in this object won't be reflected, but error won't be thrown either
+// Object.freeze(user); // further changes to any values in this object won't be reflected, but error won't be thrown either
 user.email = "abcd";
 console.log(user);
+
+user.greeting = function () {
+    console.log("Hello world!"); // function is a non-primitive datatype, so greeting is of type function
+}
+user.greeting2 = function () {
+    console.log(`Hello ${this.name}`); // function is a non-primitive datatype, so greeting is of type function
+    // use of "this" keyword is the same as java
+    // console.log("Hello ${this.name}"); // note that this does not allow string interpolation, i.e., using placeholder for objects. Quotes will convert everything to string blindly
+}
+
+console.log(user.greeting); // displays output: [Function (anonymous)]
+// function has not been executed, only its reference has been returned
+
+console.log(user.greeting()); // displays output:  Hello World! \n undefined
 
 // through constructor
 // Object.create();
